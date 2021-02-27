267251662602060b71ffb19cfe6290f852697cdd
Vincent <vl5604@rit.edu>
2021-02-27 15:39:03 -0500

    Moved int main() to HW2.cpp

--- HW2/HW2/HW2.cpp
+++ HW2/HW2/HW2.cpp
@@ -3,89 +3,72 @@
 
 #include <iostream>
 #include "snake.h";
-/*
-// This is a simple example of building and running a simulation
-// using Box2D. Here we create a large ground box and a small dynamic
-// box.
-// There are no graphics for this example. Box2D is meant to be used
-// with your rendering engine in your game engine.
+
+
+//introduce game
+//create physics world (box2d)
+//create snake (physics body b2body)
+//create while loop to keep running until user hits ESC or total # of targets
+//	inside while loop check if keypress (kbhit)
+//	if keypress then applyForces()
+//physcis world will auto update snake based on gravity/ other forces
+//
 int main()
 {
+
+
+	srand(time(0));
+	cout << "Welcome to Gravity Snake!" << endl;
+	cout << "Controls are simple, use WASD to apply a force to move your snake" << endl;
+	cout << "Gravity will make your snake naturally fall, and your goal is to hit the target twice!" << endl;
 	
-	// Define the gravity vector.
-	b2Vec2 gravity(0.0f, -10.0f);
-
-	// Construct a world object, which will hold and simulate the rigid bodies.
-	b2World world(gravity);
-
-	// Define the ground body.
-	b2BodyDef groundBodyDef;
+	//Creates world
 	groundBodyDef.position.Set(0.0f, -10.0f);
-
-	// Call the body factory which allocates memory for the ground body
-	// from a pool and creates the ground box shape (also from a pool).
-	// The body is also added to the world.
+	b2PolygonShape groundBox;
+	groundBox.SetAsBox(50.0f, 10.0f);
 	b2Body* groundBody = world.CreateBody(&groundBodyDef);
+	groundBody->CreateFixture(&groundBox, 0.0f);
 
-	// Define the ground box shape.
-	b2PolygonShape groundBoxTest;
 
-	// The extents are the half-widths of the box.
-	groundBoxTest.SetAsBox(50.0f, 10.0f);
-
-	// Add the ground fixture to the ground body.
-	groundBody->CreateFixture(&groundBoxTest, 0.0f);
-
-	// Define the dynamic body. We set its position and call the body factory.
-	b2BodyDef bodyDef;
-	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set(0.0f, 4.0f);
-	b2Body* body = world.CreateBody(&bodyDef);
-
-	// Define another box shape for our dynamic body.
+	// Creates dynamic body (snake)
+	playerSnake.type = b2_dynamicBody;
+	playerSnake.position.Set(0.0f, 100);
+	snakeBody = world.CreateBody(&playerSnake);
 	b2PolygonShape dynamicBox;
 	dynamicBox.SetAsBox(1.0f, 1.0f);
-
-	// Define the dynamic body fixture.
 	b2FixtureDef fixtureDef;
 	fixtureDef.shape = &dynamicBox;
-
-	// Set the box density to be non-zero, so it will be dynamic.
 	fixtureDef.density = 1.0f;
-
-	// Override the default friction.
 	fixtureDef.friction = 0.3f;
+	snakeBody->CreateFixture(&fixtureDef);
 
-	// Add the shape to the body.
-	body->CreateFixture(&fixtureDef);
+	// Creates target (static)
+	target.type = b2_staticBody;
+	target.position.Set((rand() / (RAND_MAX/10.0f))-5.0f, (rand() / (RAND_MAX / 10.0f)));
+	//target.position.Set(0, 50);
+	targetBody = world.CreateBody(&target);
+	b2PolygonShape targetShape;
+	targetShape.SetAsBox(1.0f, 1.0f);
+	targetBody->CreateFixture(&targetShape, 0.0f);
 
-	// Prepare for simulation. Typically we use a time step of 1/60 of a
-	// second (60Hz) and 10 iterations. This provides a high quality simulation
-	// in most game scenarios.
-	float32 timeStep = 1.0f / 60.0f;
-	int32 velocityIterations = 6;
-	int32 positionIterations = 2;
-
-	// This is our little game loop.
-	for (int32 i = 0; i < 60; ++i)
-	{
-		// Instruct the world to perform a single step of simulation.
-		// It is generally best to keep the time step and iterations fixed.
-		world.Step(timeStep, velocityIterations, positionIterations);
-
-		// Now print the position and angle of the body.
-		b2Vec2 position = body->GetPosition();
-		float32 angle = body->GetAngle();
-
-		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+	while (targetsHit < 2) {
+		// Exits while loop if Esc is hit
+		if (_kbhit()) {
+			key = _getch();
+			if (key == 27) {
+				break;
+			}
+			else
+			{
+				applyForces();
+			}
+		}
+		update();
+		display();
 	}
-
-	// When the world destructor is called, all bodies and joints are freed. This can
-	// create orphaned pointers, so be careful about your world management.
-
 	return 0;
+
 }
-**/
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
--- HW2/HW2/snake.h
+++ HW2/HW2/snake.h
@@ -3,6 +3,15 @@
 #include "Box2D.h";
 #include <conio.h>
 using namespace std;
+extern b2BodyDef groundBodyDef;
+extern b2BodyDef playerSnake;
+extern b2BodyDef target;
+extern b2Vec2 gravity;
+extern b2World world;
+extern int targetsHit;
+extern int key;
+extern b2Body* snakeBody;
+extern b2Body* targetBody;
 
 void update();
 
