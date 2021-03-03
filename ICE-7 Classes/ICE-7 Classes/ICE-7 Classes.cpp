692cee069d8ab1f18dfdcde5127248ffb5422787
Vincent <vl5604@rit.edu>
2021-03-03 17:43:56 -0500

    Finished PE 7

---
+++ ICE-7 Classes/ICE-7 Classes/ICE-7 Classes.cpp
@@ -0,0 +1,36 @@
+// ICE-7 Classes.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#include "Player.h";
+using namespace std;
+
+int main()
+{
+	Player player1 = Player();
+	Player player2 = Player((char*)"Bob", 11, 12, 13);
+	Player* player3 = new Player();
+	Player* player4 = new Player((char*)"Alice", 14, 15, 16);
+
+	player1.printPlayer();
+	player2.printPlayer();
+	player3->printPlayer();
+	player4->printPlayer();
+
+	delete player3;
+	delete player4;
+
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ ICE-7 Classes/ICE-7 Classes/Player.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+class Player
+{
+public:
+	Player(char* name, int s, int v, int d);
+	Player();
+
+	void printPlayer();
+
+private:
+	char* name;
+	int str;
+	int vit;
+	int dex;
+};
