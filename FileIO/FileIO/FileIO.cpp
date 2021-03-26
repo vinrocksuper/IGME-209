bb3986326cf439667cfb571fcbad9a7969375b92
Vincent <vl5604@rit.edu>
2021-03-26 12:19:35 -0400

    File IO PE finished

---
+++ FileIO/FileIO/FileIO.cpp
@@ -0,0 +1,39 @@
+// FileIO.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace  std;
+int main()
+{
+	ofstream myfile;
+	myfile.open("example.txt");
+	myfile << "Lorem Ipsum";
+	myfile.close();
+	
+	ifstream fileReader;
+	char* words = new char[25];
+	fileReader.open("example.txt");
+	if(fileReader.is_open())
+	{
+		fileReader.getline(words, 25);
+	}
+	
+	for (int i=0;i<25;i++)
+	{
+		cout << words[i];
+	}
+	
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
