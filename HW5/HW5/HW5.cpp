3e52ae9ad9cec110c3fcc4be07e331af24071382
Vincent <vl5604@rit.edu>
2021-04-26 20:05:23 -0400

    LNK2019 YYYYYYYYYY

--- HW5/HW5/HW5.cpp
+++ HW5/HW5/HW5.cpp
@@ -5,7 +5,7 @@
 #include "queue.h";
 int main()
 {
-	queue<int> intQ;
+	queue<int>  intQ;
 
 	int x = 0;
 	int y = 1;
@@ -20,24 +20,13 @@ int main()
 	intQ.Push(yptr);
 	intQ.Push(zptr);
 
-	intQ.Print();
-	cout << "There are " << intQ.GetSize() << " elements in the queue" << endl;
+//	intQ.Print();
+//	cout << "There are " << intQ.GetSize() << " elements in the queue" << endl;
 
 	
-	intQ.Pop();
+//	intQ.Pop();
 
-	intQ.Print();
+//	intQ.Print();
 	
     
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
